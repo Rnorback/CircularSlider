fe1c391e1d892076eafe37aa58f506c95215045b
rnorback <rnorback@gmail.com>
2016-01-09 00:19:34 -0700

    Initial Commit

---
+++ Circular Slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Circular Slider
+//
+//  Created by Rob Norback on 1/9/16.
+//  Copyright © 2016 Rob Norback. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
